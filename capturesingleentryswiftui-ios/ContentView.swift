c8f3e02075ce6d83dc5a676e24564c819cfffa12
Taif Al Musabe <taif@nsales.dk>
2021-11-09 15:33:29 +0600

    Initial Commit

---
+++ capturesingleentryswiftui-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  capturesingleentryswiftui-ios
+//
+//  Created by BS-272 on 9/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ capturesingleentryswiftui-ios/capturesingleentryswiftui_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  capturesingleentryswiftui_iosApp.swift
+//  capturesingleentryswiftui-ios
+//
+//  Created by BS-272 on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct capturesingleentryswiftui_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
